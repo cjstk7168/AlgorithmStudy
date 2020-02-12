4b10b810b0ecac0b748f9634e46c285f0bfb4d02
zynkn <wls5563@gmail.com>
2020-02-12 10:00:42 +0900

    stduy2

--- study2_200211/zynkn.js
+++ study2_200211/zynkn.js
@@ -24,28 +24,31 @@ console.log(sugar(99));
 
 
 function LRU(size, cities){
-  //const obj = {}; // 처음엔 오브젝트로 해볼까 생각함, 오브젝트를 사용하면 O(1)로 값을 찾을 수 있기 때문,
+  //const obj = {}; // 처음엔 오브젝트로 해볼까 생각함, O(1)로 값을 찾을 수 있기 때문,
   const CACHED = [];
   let total = 0;
   for(let i = 0; i < cities.length; i++){
+    cities[i] = cities[i].toLowerCase();
     if(CACHED.length < size){
       CACHED.push(cities[i]);
       total+=5; // 캐시가 없으므로 캐시 미스
     }else{
-      let pos = CACHED.indexOf(cities[i]); // 캐시에 존재하는지? 없다면 -1
-      if(pos !== -1){
-        let item = CACHED.splice(pos,1);
-        CACHED.push(...item);
-        total+=1; // 캐시 힛
-      }else{
+      let pos = CACHED.indexOf(cities[i]); // 캐시에 존재하는지? 있다면 해당 위치,  없다면 -1
+      if(pos === -1){
         CACHED.shift();
         CACHED.push(cities[i]);
         total+=5; // 캐시 미스
+      }else{
+        let item = CACHED.splice(pos,1);
+        CACHED.push(...item);
+        total+=1; // 캐시 힛
+        
       }
     }
   }
   return total;
 }
+
 
 console.log(LRU(3, ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']))
 console.log(LRU(3, ['Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul'] ))
