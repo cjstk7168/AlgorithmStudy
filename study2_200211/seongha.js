e868bbaf4c78de45692dd10b564bce8820e8acb7
Seongha <gotppetto@gmail.com>
2020-02-13 09:59:47 +0900

    Fixes ref#5 answer 1

--- study2_200211/seongha.js
+++ study2_200211/seongha.js
@@ -1,11 +1,14 @@
-const sugar = (n) => {
-  const MIN = 3, MAX = 5, LIMIT = Math.ceil(n/MAX);
-  let count = 0;
+const sugar = n => {
+  const MIN = 3,
+    MAX = 5,
+    LIMIT = Math.ceil(n / MAX);
+  let count = 0,
+    i = 0;
 
   // 1. 5kg 한번, 3kg 한번 뺄 수 있는 최솟값을 구하고
-  if ( n < MIN+MAX ) {
+  if (n < MIN + MAX) {
     // 2. 3의 배수, 5의 배수를 제외한 나머지는 -1 리턴
-    return (n%MIN === 0) ? n/MIN : (n%MAX === 0) ? n/MAX : -1;
+    return n % MIN === 0 ? n / MIN : n % MAX === 0 ? n / MAX : -1;
   }
 
   // 최소 5를 한번 이상 뺐음, 나머지는 5보다 작음
@@ -13,20 +16,19 @@ const sugar = (n) => {
   // 나머지 2 ? 5 - 3 = 2 === (5+)5 === 10
   // 나머지 4 ? 7 - 3 = 4 === (5+)7 === 12
 
-  // 3. 5kg 봉지로 배달하는 게 최소 개수이므로 최소 개수만큼 반복
-  for (let i = 0; i < LIMIT; i++) {
-    if (n >= MAX) {
-      // 4. n 값이 5보다 클때 -5kg를 뺀다
-      n -= MAX;
-    } else if (n >= MIN) {
-      // 6. n 은 5보단 작지만 3보다 같거나 작은 경우 나머지가 4, 3인 경우
-      count++;
-      return count = count + (n%MIN === 0 ? 0 : n%MIN);
-    } else {
-      return count = count + n;
+  // 3. 제일 큰 5kg 봉지로 배달하는 게 최소 배송이므로 최소 개수만큼 반복
+  while (i < LIMIT) {
+    switch (n > 0) {
+      case n >= MAX:
+        n -= MAX;
+        break;
+      case n >= MIN:
+        count = count + (n % MIN === 0 ? 0 : n % MIN);
+        break;
     }
 
     count++;
+    i++;
   }
 
   console.log(count);
